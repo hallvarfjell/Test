ceb855a951506d0e4344329269e4db91ee56bfbe
hallvarfjell <hallvarfjell@hotmail.com>
2026-02-16 16:01:23 +0100

    Delete components directory

--- components/storage.js
+++
@@ -1,5 +0,0 @@
-const Storage = {
-  load(key, def){ try{ const v=localStorage.getItem('INTZ_'+key); return v? JSON.parse(v): def; }catch(e){ return def; } },
-  save(key, val){ try{ localStorage.setItem('INTZ_'+key, JSON.stringify(val)); }catch(e){} },
-  push(key, item){ const arr=this.load(key, []); arr.push(item); this.save(key, arr); return arr; }
-};
