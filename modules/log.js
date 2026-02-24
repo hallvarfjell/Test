5a806346b5f370a01c57da668f2fa1b477977571
hallvarfjell <hallvarfjell@hotmail.com>
2026-02-25 00:31:32 +0100

    Add files via upload

--- modules/log.js
+++ modules/log.js
@@ -1,1 +1,13 @@
-const LogMod={ render(el,st){ el.innerHTML=''; const c=UI.h('div',{class:'card'}); c.append(UI.h('h2',{},'Logg'), UI.h('pre',{}, JSON.stringify(st.logg||[],null,2))); el.append(c); } };
+const LogMod={ render(el,st){ el.innerHTML='';
+  const c=UI.h('div',{class:'card'});
+  c.append(UI.h('h2',{},'Logg'));
+  const list=UI.h('div',{});
+  const L=(st.logg||[]).slice().reverse();
+  if(!L.length) list.append(UI.h('p',{},'Ingen økter ennå.'));
+  L.forEach(s=>{
+    const a=UI.h('a',{href:'#/result?id='+s.id}, new Date(s.endedAt).toLocaleString());
+    list.append(UI.h('div',{}, a, ' – ', s.name||'Økt'));
+  });
+  c.append(list);
+  el.append(c);
+}};
