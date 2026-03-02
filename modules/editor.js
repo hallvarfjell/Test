191a1e5f7460cd7b28256ecb23ba046a59f4d128
hallvarfjell <hallvarfjell@hotmail.com>
2026-03-02 12:54:55 +0100

    Delete modules directory

--- modules/editor.js
+++
@@ -1,6 +0,0 @@
-const Editor={ render(el,st){ el.innerHTML='';
-  const c=UI.h('div',{class:'card'});
-  c.append(UI.h('h2',{},'Editor'));
-  c.append(UI.h('p',{},'Her kan vi etter hvert lage/redigere øktplaner. Inntil videre brukes en enkel standardplan i Økt.'));
-  el.append(c);
-}};
