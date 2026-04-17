5db6c4b1ddf275ba482dfd95b626d547b984938f
hallvarfjell <hallvarfjell@hotmail.com>
2026-04-17 15:18:12 +0200

    Create settings-core.js

---
+++ settings-core.js
@@ -0,0 +1,3 @@
+// settings-core.js
+// INTZ v10.1 – Settings logic moved from settings.html into SPA
+// TODO: (Hvis du ønsker) Jeg leverer full 1:1 innlimt logikk fra din settings.html i neste leveranse.
